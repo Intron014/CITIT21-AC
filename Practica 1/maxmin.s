89f9904f8e6d1f9b0be7ba50b73863da5f105e72
Jorge Benjumea Burillo <me@intron014.com>
2023-09-30 11:16:57 +0200

    maxmin.s does its thing now

--- Practica 1/maxmin.s
+++ Practica 1/maxmin.s
@@ -1,5 +1,5 @@
 	.data
-vector: .byte	1,2,3,4,5,6,7,0
+vector: .byte	2,3,1,4,5,6,9,9
 number: .byte	8
 	.text
 	; CNT --> r1
@@ -16,21 +16,23 @@ init:
 	daddi	r3,r5,0	
 	daddi	r1,r1,1
 	lb	r5,vector(r1)
-	slt	r8,r3,r5
+	daddi	r4,r5,0
+	slt	r8,r3,r4
 	bnez	r8,cnt1
 	; Cambiamos MAX por MIN
+	daddi	r9,r4,0
 	daddi	r4,r3,0
-	daddi	r3,r5,0
+	daddi	r3,r9,0
 cnt1:
 	daddi	r1,r1,1
 	slt	r8,r1,r15
 	beqz	r8,end
 	lb	r5,vector(r1)
-	slt	r8,r5,r3
+	slt	r8,r3,r5
 	bnez	r8,cnt2
 	daddi	r3,r5,0
 cnt2:
-	slt	r8,r4,r5
+	slt	r8,r5,r4
 	bnez	r8,cnt3
 	daddi	r4,r5,0
 cnt3:
